d143251f10d530ca1d76ef81d9c969dcdbc85bd9
Leonardo Arrighi <Leonardoarrighi43@gmail.com>
2020-05-23 23:13:18 +0200

    check

--- src/lab_0x.R
+++ src/lab_0x.R
@@ -1,3 +1,16 @@
-x_n <- seq(-10, 10, length=1000)
-y_n <- dnorm(x_n, mean=1, sd=2)
-plot(x_n, y_n, type="l", lwd=2, col='blue')
+library(MPV)
+library(lattice)
+
+#a
+plot(y ~ x1, data=table.b3)
+
+#b
+xyplot(y ~ x1, group=x11, data=table.b3)
+
+#c
+b3.lm <- lm(y ~ x1*x11, data=table.b3)
+par(mfrow=c(1,4), pty="s")
+plot(b3.lm)
+
+#d
+xyplot(resid(b3.lm) ~ x7, group=x11, data=table.b3)
